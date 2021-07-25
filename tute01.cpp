7ceee407da4fd060e6dd6ce7b504bd8137dd888d
it21128936 <87465484+it21128936@users.noreply.github.com>
2021-07-25 10:22:56 +0000

    done exercise 1

--- tute01.cpp
+++ tute01.cpp
@@ -19,14 +19,16 @@ printf("Hello World") --> std::cout << "Hello World";
 
 
 #include <iostream>
+using namespace std;
 int main() 
 {
     float cm, inches;
-    std::count << "Enter a length in cm :"; 
-    std::cin>>cm;
+    cout << "Enter a length in cm :";
+    cin >> cm;
+
     inches = cm / 2.54;
-    std::count << "Length in inches is ";
-    std::count<<std::end;
+    cout << "Length in inches is "<<inches << endl;
+    
     
     return 0;
 }  
